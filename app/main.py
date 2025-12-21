97a338521a8de2306b9d8e932deef6718f2f62d5
Alina <darersthr@gmail.com>
2025-12-21 19:18:54 +0300

    add frontend and project startup instructions

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 import logging
 from app.config import load_config
 from app.observability import attach_observability
+from fastapi.middleware.cors import CORSMiddleware
+
 
 config = load_config("config.yaml") #загружаем конфигурация из yaml
 
@@ -10,6 +12,15 @@ print(config)
 log = logging.getLogger("app") #создаётся логгер приложения
 
 app = FastAPI(title=config["app"]["name"]) #создаётся FastAPI-приложение
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["x-request-id"], 
+)
 
 attach_observability(app, config) #подключаются логи и метрики
 
